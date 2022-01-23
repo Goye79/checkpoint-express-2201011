91f55024df8a41a46e4cff7359aeb8d2a4bfd07a
Gonzalo Goyeneche <gonzalogoyeneche79@gmail.com>
2022-01-23 20:43:52 -0300

    modificado:     models/todos.js 	nuevo archivo:  package-lock.json 	modificado:     test/model.js

--- models/todos.js
+++ models/todos.js
@@ -20,13 +20,30 @@ var tasks = {}; // acá vamos a guardar nuestras personas y tareas
 module.exports = {
   reset: function () {
     tasks = {}; // esta función ya esta armada :D
+  
   },
   // ==== COMPLETEN LAS SIGUIENTES FUNCIONES (vean los test de `model.js`) =====
   listPeople: function () {
     // devuelve un arreglo de personas con tareas
+    return Object.keys(tasks)
   },
+
   add: function (name, task) {
     // guarda una tarea para una persona en particular
+    if (task) {
+      tasks[name] = task
+    } 
   },
   // etc.
+  list: function (name, task) {
+    let listaTareas = [];
+    const tareasTotales = listaTareas.push(task)
+    return tareasTotales
+  },
+
+  complete: function (name, task){
+    if (task) {
+    return false
+  }}
+  
 };
